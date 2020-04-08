cc34a101d1771d0f2de6cef121feba12ab72a897
archanshahh <archanshahh@gmail.com>
2020-04-08 14:26:01 -0400

    modified aboutus and contactus page

--- src/components/about.js
+++ src/components/about.js
@@ -1,42 +1,93 @@
 import React from 'react';
+import img from '../image/aboutimg.jpg';
+import Nirmit from '../image/Nirmit.JPG';
+import Keval from '../image/Keval.jpg';
+import Preksha from '../image/Preksha.jfif';
+import Archan from '../image/Archan.jfif';
 
 
 function About() {
   return (
     <div>
-      <div className="container-fluid w-70 mt-2 mx-auto">
-        <div className="d-flex flex-column align-items-center text-center">
-        
-        <div className=" border border-dark border-3 w-50 bg-dark text-light mt-2">
-          <div className="col-12 text-center mx-auto">
-            <h4 className="mx-auto text-lg-center w-75 ">
-              Dare &amp; Defy helps organizations and entrepreneurs define and achieve their version of success purposefully and consciously.
-            </h4>
+      <div className="container-fluid w-70 mt-2">
+      
+        <div className="d-flex row col-12">
+          <div className="col-3 text-right">
+            <img src={img} alt="img" className="navbar-brand"  width="180" height="230" />
           </div>
-        </div>
-        
+              <h4 className="text-lg w-75 ">
+                Dare &amp; Defy helps organizations and entrepreneurs define and achieve their version of success purposefully and consciously.
+                <p>&nbsp;</p>
+                We look at your business operations, sales process and resources (people, skill sets, budget) 
+                to perfect the optimal mix of tactics, tools, and technologies to promote your business, grow your audience, sell more and build a community.
+                <p>&nbsp;</p>
+                Together, we work on defining what makes your business truly unique, and develop a strategic roadmap and a marketing plan to take your business from where it is, to where you want it to be.
+            
+              </h4>
+        </div> 
         <p>&nbsp;</p>
 
-        <div className="border border-dark border-3 w-50 text-dark bg-light mt-2">
-          <div className="col-12 text-center mx-auto">
-            <h4 className="mx-auto text-lg-center w-75 ">
-              We look at your business operations, sales process and resources (people, skill sets, budget) 
-              to perfect the optimal mix of tactics, tools, and technologies to promote your business, grow your audience, sell more and build a community.
-            </h4>
+        <div className="d-flex row col-12">
+          <div className="col-12">
+            <h3 className="mx-auto text-lg-center w-75 text-info">
+              Designed and Developed by: 
+            </h3>
           </div>
         </div>
 
-        <p>&nbsp;</p>
-
-        <div className=" border border-dark border-3 w-50 mt-2">
-          <div className="col-12 text-center mx-auto">
-            <h4 className="mx-auto text-lg-center w-75 text-info">
-            Together, we work on defining what makes your business truly unique, and develop a strategic roadmap and a marketing plan to take your business from where it is, to where you want it to be.
-            </h4>
+        <div className="row col-12">
+          <div className="col-6">
+            <h3 className="text-lg-right">
+              <img src={Archan} alt="img" className="rounded-circle"  width="100" height="100" />  
+            </h3>
+          </div>
+          <div className="col-6">
+            <h3 className="text-lg-left text-info mt-4">
+              <a href="https://www.linkedin.com/in/archanshahh/" className="text-success">Archan Shah</a> <br></br>
+            </h3>
           </div>
         </div>
 
+        <div className="row col-12">
+          <div className="col-6">
+            <h3 className="text-lg-right">
+            <img src={Keval} alt="img" className="rounded-circle"  width="100" height="100" />  
+            </h3>
+          </div>
+          <div className="col-6">
+            <h3 className="text-lg-left text-info mt-4">
+              <a href="https://www.linkedin.com/in/keval-shah-67b74616b/" className="text-success">Keval Shah</a> <br></br>
+            </h3>
+          </div>
         </div>
+
+        <div className="row col-12">
+          <div className="col-6">
+            <h3 className="text-lg-right">
+              <img src={Nirmit} alt="img" className="rounded-circle"  width="100" height="100" /> 
+            </h3>
+          </div>
+          <div className="col-6">
+            <h3 className="text-lg-left text-info mt-4">
+              <a href="https://www.linkedin.com/in/nirmit-patel-7b1562196/" className="text-success">Nirmit Patel</a> <br></br>
+            </h3>
+          </div>
+        </div>
+
+        <div className="row col-12">
+          <div className="col-6">
+            <h3 className="text-lg-right">
+              <img src={Preksha} alt="img" className="rounded-circle"  width="100" height="100" />  
+            </h3>
+          </div>
+          <div className="col-6">
+            <h3 className="text-lg-left text-info mt-4">
+              <a href="https://www.linkedin.com/in/prekshapatel18/" className="text-success">Preksha Patel</a> <br></br>
+            </h3>
+          </div>
+        </div>
+
+        
       </div>
     </div>
   );
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,60 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Map from './maps';
 
-
-import { BrowserRouter as Router, Route} from "react-router-dom";
-import About from './about';
-
-
-function Contact() {
-  return (
-    <div>
-      <div className="container-fluid w-70 mt-2 mx-auto">
-        <div className="d-flex flex-column align-items-center text-center">
-        
-        <div className=" border border-dark border-3 w-50 bg-dark text-light mt-2">
-          <div className="col-12 text-center mx-auto">
-            <h4 className="mx-auto text-lg-center w-75 ">
-              We’re located at:
-            </h4>
-            <p>&nbsp;</p>
-            <h6 className="mx-auto text-lg-center w-75 ">
-              1179 King Street West, Unit 008 <br></br>
-              Toronto, ON&nbsp; M6K 3C5
-            </h6>
+export class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+        <div className=" row col-12">
+          <div className="col-6">
+            <div className="container-fluid mt-4">
+              <div className="text-light mt-2">
+                <div className="col-12 text-left">
+                  <h4 className="mx-auto text-lg-left ">
+                    We’re located at:
+                    </h4>
+                  <p>&nbsp;</p>
+                  <h6 className="text-lg-left">
+                    1179 King Street West, Unit 008 <br></br>
+                      Toronto, ON&nbsp; M6K 3C5
+                    </h6>
+                  <p>&nbsp;</p>
+                  <h4 className="mx-auto text-lg-left d-inline-block">
+                    Email: <a href="mailto:hello@dndstrategy.com">hello@dndstrategy.com</a>
+                  </h4>
+                  <h4 className="mx-auto text-lg-left d-inline-block">
+                    <p>Phone: 416.919.6817</p>
+                  </h4>
+                  <p>&nbsp;</p>
+                  <h4 className="mx-auto text-lg-left text-info d-inline-block">
+                    Follow us on: <br></br>
+                    <a href="https://www.instagram.com/dndstrategy/" className="text-success">Instagram @dndstrategy</a> <br></br>
+                    <a href="https://www.facebook.com/dndstrategy/" className="text-success">Facebook @dndstrategy</a> <br></br>
+                    <a href="https://www.linkedin.com/company/d&amp;d-strategy/" className="text-success">LinkedIn Dare &amp; Defy</a> <br></br>
+                  </h4>
+                </div>
+              </div>
+            <br/><br/><br/>
+            </div>
           </div>
-        </div>
-        
-        <p>&nbsp;</p>
-
-        <div className="border border-dark border-3 w-50 text-dark bg-light mt-2">
-          <div className="col-12 text-center mx-auto">
-            <h4 className="mx-auto text-lg-center w-75 ">
-              email: <a href="mailto:hello@dndstrategy.com">hello@dndstrategy.com</a>  
-            </h4>
-            <h4 className="mx-auto text-lg-center w-75 ">
-              <p>phone: 416.919.6817</p>  
-            </h4>
-            
+          <div className="col-6 mt-3">
+            <Map />
           </div>
-        </div>
-
-        <p>&nbsp;</p>
-
-        <div className=" border border-dark border-3 w-50 mt-2">
-          <div className="col-12 text-center mx-auto">
-            <h4 className="mx-auto text-lg-center w-75 text-info">
-              Follow us on: <br></br>
-              <a href="https://www.instagram.com/dndstrategy/" className="text-success">Instagram @dndstrategy</a> <br></br>
-              <a href="https://www.facebook.com/dndstrategy/" className="text-success">Facebook @dndstrategy</a> <br></br>
-              <a href="https://www.linkedin.com/company/d&amp;d-strategy/" className="text-success">LinkedIn Dare &amp; Defy</a> <br></br>
-            </h4>
-          </div>
-        </div>
-        <br/><br/><br/>
         </div>
       </div>
-    </div>
+      <div className="mt-5"></div>
+    </div >
   );
+  }
 }
 
 export default Contact;
