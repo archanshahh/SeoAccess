9fda6b66c8f65bb389815aa6a07871b4a4aaa0bc
archanshahh <archanshahh@gmail.com>
2020-04-14 13:11:34 -0400

    modified main.js with form labels

--- src/components/main.js
+++ src/components/main.js
@@ -69,11 +69,12 @@ class FirstPage extends React.Component {
                         <h1 style={{ color: "white" }}>
                             Why SeoAccess?
                         </h1>
-                        <p>How fast and accessible is your website?</p>
+                        <p>We provide modern SEO & Web accessibility checks for your webpage!</p>
                         <div className="border border-dark border-3 w-50 textcolor mt-2">
                             <div className="container-fluid space">
                                 <div className="row">
                                     <div className="col-md-6 col-sm-6 col-xs-12 vr jus">
+                                        <h3>SEO</h3>
                                         <p>SEO results in increased traffic and conversions.
                                                 SEO allows companies to get traffic that can drive conversions and revenue. Rather than
                                                 spending money on social media ads and other marketing tactics, we suggest building out SEO.
@@ -82,6 +83,7 @@ class FirstPage extends React.Component {
                                     </div>
 
                                     <div className="col-md-6 col-sm-6 col-xs-12 jus">
+                                        <h3>Web Accessibility</h3>
                                         <p>
                                             It is important that the Web is open to all so that people with different abilities have fair access and equal opportunities.
                                             An accessible web can help people with different abilities to participate more actively in society.
@@ -96,35 +98,35 @@ class FirstPage extends React.Component {
 
                     <form className="centre-block mt-4 mx-auto w-50" onSubmit={this.onSubmit}>
                         <div className="form-group">
+                            <label for="name" hidden>Name : </label>
                             <input type="text" required
                                 className="form-control box"
                                 id="name"
                                 placeholder="Enter your name"
                                 value={this.state.name}
                                 onChange={this.onChangeName}
-                            ></input>
+                            />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group"><label for="email" hidden>Email : </label>
                             <input type="email" required className="form-control box" id="email" placeholder="name@example.com "
                                 value={this.state.email}
                                 onChange={this.onChangeEmail}
-                            >
+                            />
 
-                            </input>
                         </div>
-                        <div className="form-group">
+                        <div className="form-group"><label for="url" hidden>URL : </label>
                             <input type="url" required className="form-control box" id="url" placeholder="www.url.com" value={this.state.url}
                                 onChange={this.onChangeUrl}
 
-                            ></input>
+                            />
                         </div>
-                        <div className="form-check">
+                        <div className="form-check"><label for="privacy" hidden></label>
                             <input type="checkbox" className="form-check-input" id="privacy" required />
                             <label className="form-check-label textcolor" htmlFor="privacy">I accept privacy policy</label>
                         </div>
 
-                        <div className="form-group">
-                            <button type="submit" className="btn btn-primary d-block mt-4 mx-auto">See report</button>
+                        <div className="form-group"><label for="sub" hidden></label>
+                            <button type="submit" id="sub" className="btn btn-primary d-block mt-4 mx-auto">See report</button>
                         </div>
                     </form>
                     <label id="loading" hidden>Loading ...</label>
